138445db1eff4498fb7e5270ce76adb04207965a
佛系豪豪吖 <85242109+foxihaohao@users.noreply.github.com>
2022-09-17 12:56:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -68,17 +68,17 @@ export const config = {
       // 所在省份
       province: "河南",
       // 所在城市
-      city: "民权",
+      city: "许昌",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '02-15',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://www.zxxyedu.cn/,
+      openUrl: "",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
+        {"type": "*生日", "name": "老婆", "year": "2000", "date": "02-15"},
         {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
@@ -93,43 +93,19 @@ export const config = {
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
-    },
-    {
-      name: "测试成功！",
-      id: "o60yp6P_xgJNHNRkB3I52R4l6wXU",
-      useTemplateId: "INRaliF-bY52F_gF9ZLYvkkMMLFb3fVTCwwPLC1PT9o",
-      province: "河南",
-      city: "许昌",
-      horoscopeDate: '02-15',
-      horoscopeDateType: '今日',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
-    },
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
-    },
-    {
-      name: "老婆3",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
-    },
+    }
+//     {
+//       name: "测试成功！",
+//       id: "o60yp6P_xgJNHNRkB3I52R4l6wXU",
+//       useTemplateId: "INRaliF-bY52F_gF9ZLYvkkMMLFb3fVTCwwPLC1PT9o",
+//       province: "河南",
+//       city: "许昌",
+//       horoscopeDate: '02-15',
+//       horoscopeDateType: '今日',
+//       openUrl: "https://wangxinleo.cn",
+//       festivals: [],
+//       customizedDateList: []
+//     }
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -237,6 +213,4 @@ export const config = {
     // 你可以不断按格式往下增加
     // ...
   ],
-
-
 }
